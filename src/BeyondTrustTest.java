2d341207f0b8d4563d8ae58d7dcc7ac14b5681d0
existential1 <cyoungns@gmail.com>
2024-06-18 18:14:38 -0300

    updates

--- src/BeyondTrustTest.java
+++ src/BeyondTrustTest.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -26,6 +27,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 public class BeyondTrustTest {
 
 	private WebDriver driver;
+	ChromeOptions options = new ChromeOptions();
 	List<String> requiredFields = new ArrayList<>(Arrays.asList("title", "applicationArea", "description", "dateTime"));
 	List<String> requiredFieldValues = new ArrayList<>(Arrays.asList("Title", "Settings", "Description", "01/01/1970"));
 	private final String SIMPLE_APP_BASE_URL = "http://localhost";
@@ -38,6 +40,11 @@ public class BeyondTrustTest {
 	private final String PROJECT_BASE_PATH = "/git/beyond-trust";
 	private final String CREATE_TICKET_XPATH = "//*[@href='/create']";
 	private final String SUBMIT_TICKET_XPATH = "/html/body/app-root/app-item/app-item-form/form/button";
+	private final String STATUS_MESSAGE_CSS_SELECTOR = "div.mat-mdc-snack-bar-label.mdc-snackbar__label";
+	private final String CREATE_SUCCESS_MESSAGE = "Item created successfully";
+	private final String DELETE_SUCCESS_MESSAGE = "Item deleted successfully";
+	private final String ERROR_MESSAGE = "Error occurred, check the network tab on your browser's developer tools for more details";
+	private final String BAD_VIDEO_URL_INPUT = "blah";
 	private static final int POLLING = 60;
 	private WebDriverWait wait;
 	
@@ -45,6 +52,7 @@ public class BeyondTrustTest {
 	private void init() throws IOException, InterruptedException {
 		boolean isWindows = System.getProperty("os.name")
 				  .toLowerCase().startsWith("windows");
+		System.setProperty("webdriver.chrome.driver", HOME_DIR + PROJECT_BASE_PATH + CHROMEDRIVER_PATH);
 		ProcessBuilder builder = new ProcessBuilder();
 		if (isWindows) {
 			builder.command("cmd.exe", "/c", HOME_DIR + PROJECT_BASE_PATH + DOCKER_SCRIPT_PATH);
@@ -62,51 +70,52 @@ public class BeyondTrustTest {
 	
 	@BeforeMethod
 	private void setup() {
-		System.setProperty("webdriver.chrome.driver", HOME_DIR + PROJECT_BASE_PATH + CHROMEDRIVER_PATH);
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--remote-allow-origins=*");
-		driver = new ChromeDriver(options);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(POLLING));
-		this.wait = new WebDriverWait(driver, Duration.ofSeconds(POLLING));
-		driver.manage().window().maximize();
-		driver.get(SIMPLE_APP_BASE_URL + SIMPLE_APP_PORT_DELIMITER + SIMPLE_APP_PORT);
+	
 	}
 	
 	@Test
-	private void createTicket_required_fields_filled () {
-		System.out.printf("Test 1\n");
-		
+	private void createTicket_succeed_required_fields_filled () {
+		initializeDriver();
 		driver.findElement(By.xpath(CREATE_TICKET_XPATH)).click();
-		
-		for(int i = 0; i < requiredFields.size(); i++) {
-			setTicketField(requiredFields.get(i),requiredFieldValues.get(i)); 
-		}
+		populateRequiredFields(-1);
 		driver.findElement(By.xpath(SUBMIT_TICKET_XPATH)).click();
-		wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mdc-list-item__content")));
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//span[text() = ' Dismiss ']")));
-		//wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("mdc-button__label")));
+		Assert.assertFalse(checkForRequiredFieldError());
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(STATUS_MESSAGE_CSS_SELECTOR)));
+		String message = driver.findElement(By.cssSelector(STATUS_MESSAGE_CSS_SELECTOR)).getText();
+		Assert.assertEquals(message, CREATE_SUCCESS_MESSAGE);
 	}
 	
 	@Test
-	private void createTicket_required_field_empty () {
-		System.out.printf("Test 2\n");
+	private void createTicket_fail_required_field_empty () {
+		initializeDriver();
+		driver.findElement(By.xpath(CREATE_TICKET_XPATH)).click();
+		populateRequiredFields(1);
+		driver.findElement(By.xpath(SUBMIT_TICKET_XPATH)).click();
+		Assert.assertTrue(checkForRequiredFieldError());					
 	}
 	
 	@Test
-	private void createTicket_optional_field_invalid () {
-		System.out.printf("Test 3\n");
+	private void createTicket_fail_optional_field_invalid () {
+		initializeDriver();
+		driver.findElement(By.xpath(CREATE_TICKET_XPATH)).click();
+		populateRequiredFields(-1);
+		setTicketField("videoUrl", BAD_VIDEO_URL_INPUT);
+		driver.findElement(By.xpath(SUBMIT_TICKET_XPATH)).click();
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(STATUS_MESSAGE_CSS_SELECTOR)));
+		String message = driver.findElement(By.cssSelector(STATUS_MESSAGE_CSS_SELECTOR)).getText();
+		Assert.assertEquals(message, ERROR_MESSAGE);		
 	}
 	
 	private void setTicketField(String field, String value) {
 		WebElement el = driver.findElement(By.xpath("//*[@formcontrolname='" + field + "']"));
 		el.click();
-		el.sendKeys(value);
 		switch (field) {
 		case "title":
 		case "description":
 		case "dateTime":
 		case "videoUrl":
 		case "contactEmail":
+			el.sendKeys(value);
 			break;
 		case "applicationArea":
 			driver.findElement(By.xpath("//span[text() = '" + value + "']")).click();
@@ -115,6 +124,40 @@ public class BeyondTrustTest {
 			el.sendKeys(Keys.ENTER);
 			break;
 		}
+	}
+	
+	private void initializeDriver() {
+		options.addArguments("--remote-allow-origins=*");
+		driver = new ChromeDriver(options);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(POLLING));
+		this.wait = new WebDriverWait(driver, Duration.ofSeconds(POLLING));
+		driver.manage().window().maximize();
+		driver.get(SIMPLE_APP_BASE_URL + SIMPLE_APP_PORT_DELIMITER + SIMPLE_APP_PORT);		
+	}
+	
+	private void populateRequiredFields(int skip) {
+		for(int i = 0; i < requiredFields.size(); i++) {
+			if(i == skip) {
+				continue;
+			}
+			setTicketField(requiredFields.get(i),requiredFieldValues.get(i)); 
+		}
+	}
+	
+	private boolean checkForRequiredFieldError() {
+		boolean error = false;
+		
+		WebElement wel = driver.findElement(By.xpath("/html/body/app-root/app-item/app-item-form/form"));
+		if(wel.getAttribute("class").contains("ng-invalid")) {
+			List<WebElement> fields = driver.findElements(By.tagName("mat-form-field"));
+			for (WebElement field : fields) {
+				if(field.getAttribute("class").contains("ng-invalid")) {
+					error = true;
+				}
+			}
+		}
+		
+		return(error);
 	}
 	
 	@AfterMethod
